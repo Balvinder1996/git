5b3852ce9ba72dd15626cab793477924bde37c26
balvindersingh <balvindersingharora7@gmail.com>
2021-05-29 16:39:06 +0530

    pure commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -12,13 +12,13 @@ const Home=()=>
                 <div className="col-lg-7 d-flex align-items-center flex-column justify-content-center" id="text">
                     
                         <h1 className="h1 ">Welcome to Rose's Cafe</h1>
-                        <p className="display-4 animate__animated animate__flipOutX animate__infinite animate__slow ">Welcome here</p>
+                        <p className="display-4  ">Welcome here</p>
                    
                 </div>
             </div>
             <div className="row h-25" id="submain">
                 <div className="col-lg-4 d-flex justify-content-center align-items-center">
-                    <h1 className="animate__animated animate__heartBeat animate__infinite animate__slow">12</h1>
+                    <h1>12</h1>
                 </div>
                 <div className="col-lg-4 d-flex justify-content-center align-items-center">
                      <h1>13</h1>
